c374724f1f74865f60f061da225fc7941db548d9
Mariam <eng.mariamnabil93@gmail.com>
2023-10-04 10:52:40 -0700

    1

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for x in range(122, 96, -1):
+    if x % 2 == 0:
+        n = chr(x)
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    newStr = ""
+    for x in range(len(str)):
