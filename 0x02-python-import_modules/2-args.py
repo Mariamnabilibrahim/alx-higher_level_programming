c1acbe54646ae8b3af71330865248e69b2ae31c2
Mariam <eng.mariamnabil93@gmail.com>
2023-10-05 10:12:57 -0700

    1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     """prints the number of and the list of its arguments."""
     import sys
 
-    c = len(sys.argv) - 1
-    if c == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
