8727e73e6638186afc299ff8e25dc6cc5f604bc7
Mariam <eng.mariamnabil93@gmail.com>
2023-11-07 09:31:40 -0800

    1

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,6 +2,7 @@
 """definig write_file with two arguments"""
 
 
+
 def append_write(filename="", text=""):
     """reads filename with UTF-8"""
     with open(filename, "a", encoding="UTF-8") as f:
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+""" defining function """
 
 
 import json
 
 
 def to_json_string(my_obj):
+    """return function of an object"""
     return json.dumps(my_obj)
