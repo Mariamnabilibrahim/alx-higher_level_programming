d2ab5b48e9664cd57e8bf32689a463bd24fe5955
Mariam <eng.mariamnabil93@gmail.com>
2023-11-07 10:02:16 -0800

    1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""defines function to read text file and prints to stdout"""
 
 
 def read_file(filename=""):
-    with open(filename, encoding="UTF-8") as f:
+    """reads text file and prints to stdout"""
+    with open(filename) as f:
         print(f.read(), end="")
